d7e5d3f5301d2f62fc43759b1e8f0cec11778b44
Skiba-front <58513588+Skiba-front@users.noreply.github.com>
2021-01-22 04:54:24 +0500

    feat: add towel sort function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let res = []
+
+  for(let i = 0; i < matrix.length; i++) {
+    for(let j = 0; j < matrix.length; j++) {
+      res.push(matrix[i][j])
+    }
+  }
+
+  return res.sort((a, b) => a - b)
 }
