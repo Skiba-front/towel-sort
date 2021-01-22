ad823ff3e36f121e3ed8933caa581dffbcc4cae5
Skiba-front <58513588+Skiba-front@users.noreply.github.com>
2021-01-22 05:04:18 +0500

    feat: add empty matrix checking

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+     if (!matrix) {
+    return [];
+     }
   let res = []
 
   for(let i = 0; i < matrix.length; i++) {
